5c83e287d7e784046d2cd17e3b2905d2e0c5a12f
WRogoski <143140187+WRogoski@users.noreply.github.com>
2024-05-20 23:32:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,50 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from prophet import Prophet
+import plotly.graph_objects as go
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+
+from prophet.diagnostics import cross_validation
+
+import requests
+from bs4 import BeautifulSoup
+
+# URL do site IPEADATA
+url = 'http://www.ipeadata.gov.br/ExibeSerie.aspx?module=m&serid=1650971490&oper=view'
+
+# Faz uma requisição GET ao site e captura a resposta
+response = requests.get(url)
+
+# Verifica se a requisição foi bem sucedida
+if response.status_code == 200:
+    # Cria um objeto BeautifulSoup para analisar o HTML
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Procura pela tabela no HTML analisado (o id ou classe pode variar)
+    # Você precisaria inspecionar o HTML para obter o seletor correto
+    table = soup.find('table', {'id': 'grd_DXMainTable'})
+
+    # Usa o pandas para ler a tabela HTML diretamente para um DataFrame
+    df = pd.read_html(str(table),skiprows=0)[0]
+
+    df.columns = df.iloc[0]
+    df = df.drop(0)
+
+    # Mostra as primeiras linhas do DataFrame
+    df.head()
+else:
+    print('Falha ao acessar a página: Status Code', response.status_code)
+
+df['Preço - petróleo bruto - Brent (FOB)'] = df['Preço - petróleo bruto - Brent (FOB)'].astype(float)
+df['Preço - petróleo bruto - Brent (FOB)'] = df['Preço - petróleo bruto - Brent (FOB)']/100
+df.head()
+
 # Definição das funções para cada guia do aplicativo
 def tab1():
     st.title('Tech Challenge')
@@ -56,16 +103,114 @@ def tab2():
     st.markdown('A invasão russa à Ucrânia foi o principal fator de aumento recente ao preço do barril. O ataque criou incertezas significativas no mercado de energia, já que muitas sanções foram impostas à Russia, uma das principais fontes globais de petróleo. Além do conflito do leste europeu, tensões envolvendo os países do Oriente Médio, outra importante fonte do produto, afetaram a estabilidade da oferta e dos preços.')
 
 
+
+#Renomear as colunas identificadas para o padrão 'ds' (Datestamp) e 'y' (target)
+df.rename(columns={'Data': 'ds', 'Preço - petróleo bruto - Brent (FOB)': 'y'}, inplace=True)
+
+#Ajustar a coluna 'ds' para formato de data
+df['ds'] = pd.to_datetime(df['ds'])
+
+#Remover nulos da coluna 'y'
+df = df.dropna(subset=['y'])
+
+#Ordenando o df por data
+df = df.sort_values(by='ds')
+
+plt.figure(figsize = (20,10))
+plt.plot(df['ds'], df['y'], label='Brent_FOB')
+
+plt.legend(loc='best')
+plt.show()
+
+
+
+
+
+#Separando as bases de treino e teste na proporção 80% 20%
+
+train_data = df.sample(frac=0.8, random_state=20)
+test_data = df.drop(train_data.index)
+print(f'training data size : {train_data.shape}')
+print(f'testing data size : {test_data.shape}')
+
+#Reduzindo tamanho do df para melhorar a visualização e realizar as análises
+df_24 = df[df['ds'].dt.year >= 2024]
+df_24 = df_24.sort_values(by='ds')
+df_24.head()
+
+#Vizualizando o comportamento do pertóleo no último ano
+plt.figure(figsize = (20,10))
+plt.plot(df_24['ds'], df_24['y'], label='Brent_FOB')
+plt.title('Brent_FOB_avg')
+plt.legend(loc='best')
+plt.show()
+
+
+#Criando uma cópia do df já limpo e ajustado para utilizar na análise, mantendo o original salvo para uso posterior
+df_2 = df_24
+
+#Definindo a data como índice para que o modelo rode corretamente
+df_2 = df_2.set_index('ds')
+
+
+#Iniciei copiando o df gerado anteriormente para realização da análise com Prophet
+df_p2 = df_24
+
+#Confirmando se os comandos foram aceitos corretamente
+df_p2.head()
+
+#Separando as bases de treino e teste na proporção 80% 20%
+
+train_data = df_p2.sample(frac=0.80, random_state=20)
+test_data = df_p2.drop(train_data.index)
+print(f'training data size : {train_data.shape}')
+print(f'testing data size : {test_data.shape}')
+
+#Instanciando o Prophet para utilizar uma sazonalidade diária e gerar previsões
+
+modelo = Prophet(daily_seasonality=True)
+modelo.fit(train_data)
+dataFramefuture = modelo.make_future_dataframe(periods=10, freq='D')
+previsao = modelo.predict(dataFramefuture)
+previsao.head()
+
+#Visualizando os dados juntamente com as previsões
+modelo.plot(previsao, figsize=(20,6));
+plt.plot(test_data['ds'], test_data['y'], '.r')
+
+modelo.plot_components(previsao, figsize=(10,6))
+
+#Extraindo as colunas relevantes dos DataFrames e mesclando os DataFrames nas colunas 'ds' para comparar previsões e valores reais
+previsao_cols = ['ds', 'yhat']
+valores_reais_cols = ['ds', 'y']
+
+previsao = previsao[previsao_cols]
+valores_reais = train_data[valores_reais_cols]
+
+resultados = pd.merge(previsao, valores_reais, on='ds', how='inner')
+
+#Calcular o erro percentual absoluto para cada ponto de dados e o MAPE
+resultados['erro_percentual_absoluto'] = np.abs((resultados['y'] - resultados['yhat']) / resultados['y']) * 100
+
+mape = np.mean(resultados['erro_percentual_absoluto'])
+
+print(f"MAPE: {mape:.2f}%")
+
+
+
+
 def tab3():
     st.title('Modelo preditivo')
-    st.markdown(f'Para construção do modelo foi utilizado o Prophet, que, considerando a base de preços de 2024, entregou a melhor predição, apresentando um MAPE de 1,38%.')
+    st.markdown(f'Para construção do modelo foi utilizado o Prophet, que, considerando a base de preços de 2024, entregou a melhor predição, apresentando um MAPE de {mape:.2f}%.')
     st.markdown('<h5> <b>Petróleo Brent (FOB) </b></h5>', unsafe_allow_html=True)
     st.markdown('Série histórica de preços do Petróleo Brent (FOB) em 2024')
-
+    imagem1 = 'https://raw.githubusercontent.com/WRogoski/brent_price/main/Brent_avg.png'
+    st.image(imagem1)
 
     
     st.markdown('<h5> <b>Modelo Preditivo </b></h5>', unsafe_allow_html=True)
-
+    imagem2 = 'https://raw.githubusercontent.com/WRogoski/brent_price/main/prophet.png'
+    st.image(imagem2)
 
 
 # Aplicativo Streamlit
